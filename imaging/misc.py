54f95b86865e558abf7d2a112ee8008b3b06548a
Csxeba <csxeba@gmail.com>
2017-01-17 16:13:02 +0100

    Fitting of Curves

--- imaging/misc.py
+++ imaging/misc.py
@@ -1,40 +1,72 @@
+import os
+
 import numpy as np
+from skimage import exposure, filters, measure, feature
+
+# NOT USED ATM
+THRESHOLDS = [1., 2., 0.]
 
 
-THRESHOLDS = [1.75, 0.6, 0.6]
-
-
-def pull_data(source=None):
+def pull_data(source=None, randomize=False, verbose=0):
     from csxdata import roots
-    from csxdata.utilities.highlevel import image_sequence_to_array
+    from csxdata.utilities.highlevel import image_to_array
 
     source = roots["ntabpics"] if source is None else source
-    return image_sequence_to_array(source, outpath=roots["cache"] + "ntabpics.npa", generator=1)
+    pics = np.array([source + fl for fl in os.listdir(source)])
+    if randomize:
+        np.random.shuffle(pics)
+    for i, path in enumerate(pics, start=1):
+        if verbose:
+            print("PIC {:>{w}}/{}".format(i, len(path), w=len(str(len(path)))))
+        yield image_to_array(path)
 
 
-def preprocess(pixels, dist=True):
-    # from matplotlib import pyplot as plt
+def preprocess(pixels, dist=True, show=True):
     from scipy import ndimage as ndi
-    from skimage import measure
+    from matplotlib import pyplot as plt
 
-    def binarize(p):
-        channel_averages = p.mean(axis=(0, 1)) * THRESHOLDS
-        tmp = np.greater_equal(p, channel_averages[None, None, :]).sum(axis=2)
-        binned = np.greater_equal(tmp, 3)
-        binit = ndi.binary_fill_holes
-        nay = np.logical_not
+    binit = ndi.binary_fill_holes
+    nay = np.logical_not
+
+    def equalize(p):
+        return exposure.equalize_adapthist(p[:, :, 0])
+
+    def binarize_by_hardcoded_thresholding(p):
+        # thresh = p.mean(axis=(0, 1)) * THRESHOLDS
+        thresh = np.array([80, 120, 256])
+        tmp = np.greater_equal(p, thresh[None, None, :]).sum(axis=2)
+        binned = np.greater_equal(tmp, 1)
+        return nay(binit(nay(binit(binned)), structure=np.ones((5, 5))))
+
+    def binarize_by_mean_thresholding(p):
+        thresh = p.mean(axis=(0, 1)) * THRESHOLDS
+        tmp = np.greater_equal(p, thresh[None, None, :]).sum(axis=2)
+        binned = np.greater_equal(tmp, 1)
+        return nay(binit(nay(binit(binned)), structure=np.ones((5, 5))))
+
+    def binarize_by_otsu_threasholding(p):
+        thresh = filters.threshold_otsu(p)
+        tmp = np.greater_equal(p, thresh)
+        return nay(binit(nay(binit(tmp))))
+
+    def binarize_by_edge_detection(p):
+        edges = sum((feature.canny(p[:, :, i] / 255.) for i in range(2)))
+        binned = np.greater_equal(edges, 2)
+        plt.imshow(binned)
+        plt.show()
         return nay(binit(nay(binit(binned)), structure=np.ones((3, 3))))
 
-    filled = binarize(pixels)
-
-    # plt.imshow(pixels)
-    # plt.show()
-    # plt.imshow(filled)
-    # plt.show()
+    eqd = equalize(pixels)
+    filled = binarize_by_otsu_threasholding(eqd)
 
     labelled, nlab = measure.label(filled, return_num=True)
 
-    assert nlab > 0
+    if show:
+        fig, axes = plt.subplots(2, 2)
+        axes = axes.ravel()
+        for i, im in enumerate((pixels, eqd, filled, labelled)):
+            axes[i].imshow(im)
+        plt.show()
 
     if not dist:
         return labelled, nlab
--- imaging/xperiment.py
+++ imaging/xperiment.py
@@ -4,29 +4,34 @@ from matplotlib import pyplot as plt
 
 from SciProjects.imaging import pull_data, preprocess, pprint
 
-
-pics = pull_data()
+source = "D:/tmp/jpegs/"
+pics = pull_data(source=source, randomize=True, verbose=1)
 
 # R-G-B
 MAX_LBLS = 3
 # picX, picY, picZ = 1944, 2592, 3
 MAXFSIZE = 50
-SCALE = 0.00858122427983539094650205761317
+SCALE = 0.0085812242798
 UPSCALE = 2.
 verbose = 1
 MINAREA = 10000
 
 
-def algo_fitpolynom(dpic, lpic, nlab, centering=None, show=0):
+def algo_fitpolynom(dpic, lpic, nlab, **kw):
+
+    show = kw.get("show", False)
+
     picX, picY = lpic.shape
-    props = [prp for prp in measure.regionprops(lpic) if prp.area > MINAREA]
-    for prp in props:
-        niceleft = prp.image.copy().astype(int)
-        niceright = prp.image.copy().astype(int)
+    prps = [prp for prp in measure.regionprops(lpic)
+            if prp.area > MINAREA or prp.image.shape[0] > 1000]
+    measurements = []
+    curves = []
+    for prp in prps:
+        x0, y0, x1, y1 = prp.bbox
         edge = np.argwhere(feature.canny(prp.image))
         leftedge, rightedge = [], []
         la, ra = leftedge.append, rightedge.append
-        for y in range(picY):
+        for y in range(y1):
             ft = [e[1] for e in edge if e[0] == y]
             if len(ft) == 0:
                 continue
@@ -34,22 +39,52 @@ def algo_fitpolynom(dpic, lpic, nlab, centering=None, show=0):
             ra((y, max(ft)))
 
         leftedge, rightedge = np.array(leftedge).T, np.array(rightedge).T
-        leftcurve = np.poly1d(np.polyfit(*leftedge, deg=5))
-        rightcurve = np.poly1d(np.polyfit(*rightedge, deg=5))
+        lefts = []
+        rights = []
+        for i in range(3, 7):
+            leftcurve, lr, _, _, _ = np.polyfit(*leftedge, deg=i, full=True)
+            rightcurve, rr, _, _, _ = np.polyfit(*rightedge, deg=i, full=True)
+            lefts.append((leftcurve, lr, i))
+            rights.append((rightcurve, rr, i))
+        lefts.sort(key=lambda tup: tup[1])
+        rights.sort(key=lambda tup: tup[1])
+        leftcurve, lr, dl = lefts[0]
+        rightcurve, rr, dr = rights[0]
+        leftcurve, rightcurve = np.poly1d(leftcurve), np.poly1d(rightcurve)
+        curves.append((leftcurve, rightcurve))
+
+        print("left R: {}, deg: {}\nright R: {}, deg: {}".format(lr, dl, rr, dr))
 
         lefti = leftcurve.integ()
         righti = rightcurve.integ()
-        area = righti - lefti
 
-        # lsp = np.linspace(0, picY, 200)
-        # plt.plot(leftedge[0], leftedge[1], ".", color="red")
-        # plt.plot(rightedge[0], rightedge[1], ".", color="blue")
-        # plt.plot(lsp, leftcurve(lsp), "--", color="red")
-        # plt.plot(lsp, rightcurve(lsp), "--", color="blue")
-        # plt.show()
+        # S <x0, x1> f(x) dx - S <x0, x1> g(x) dx
+        # ---------------------------------------
+        #                x1 - x0
+        avgdistance = ((righti(y1) - righti(y0)) - (lefti(y1) - lefti(y0))) / (y1 - y0)
+        measurements.append(avgdistance * SCALE)
+
+    if show:
+        plt.imshow(lpic)
+        lsp = np.linspace(0, picY, 200)
+        for prp, (leftcurve, rightcurve) in zip(prps, curves):
+            x0, y0, x1, y1 = prp.bbox
+            leftX = leftcurve(lsp) + y0
+            leftY = lsp + x0
+            rightX = rightcurve(lsp) + y0
+            rightY = lsp + x0
+            plt.plot(leftX, leftY, "--", color="yellow")
+            plt.plot(rightX, rightY, "--", color="yellow")
+        axes = plt.gca()
+        axes.set_xlim([0, picY])
+        axes.set_ylim([0, picX])
+        plt.show()
+    return measurements
 
 
-def algo_maxwidth(dpic, lpic, nlab, centerwith, show=0):
+def algo_maxwidth(dpic, lpic, nlab, **kw):
+    show = kw.get("show", 0)
+    centering = kw.get("centering", np.mean)
     if show:
         plt.imshow(dpic)
         plt.show()
@@ -59,7 +94,7 @@ def algo_maxwidth(dpic, lpic, nlab, centerwith, show=0):
 
     bboxes = (prp.bbox for prp in measure.regionprops(lpic) if prp.area > MINAREA)
     slices = (dpic[:, y0:y1] for x0, y0, x1, y1 in bboxes)
-    maxes = (centerwith(slc.max(axis=1)) for slc in slices)
+    maxes = (centering(slc.max(axis=1)) for slc in slices)
     measurements = [mx * SCALE * UPSCALE for mx in maxes]
 
     if len(measurements) < nlab:
@@ -70,15 +105,24 @@ def algo_maxwidth(dpic, lpic, nlab, centerwith, show=0):
 
 
 # noinspection PyUnusedLocal
-def algo_area(dpic, lpic, nlab, centering=None, show=0):
-    if show:
-        plt.imshow(lpic)
+def algo_area(dpic, lpic, nlab, **kw):
+
+    show = kw.get("show", False)
+
+    def display():
+        nosp = len(prps) + (0 if len(prps) % 2 == 0 else 1)
+        fig, axes = plt.subplots(2, nosp // 2)
+        axes = axes.ravel()
+        for i, prp in enumerate(prps):
+            axes[i].imshow(prp.image)
         plt.show()
 
-    prps = measure.regionprops(lpic)
+    prps = [prp for prp in measure.regionprops(lpic) if prp.area > MINAREA and prp.image.shape[0] > 1000]
     bboxes = (prp.bbox for prp in prps)
-    measurements = [(prp.area / (x1-x0)) * SCALE for prp, (x0, y0, x1, y1) in
-                    zip(prps, bboxes) if prp.area > MINAREA]
+    measurements = [(prp.area / (x1-x0)) * SCALE for
+                    prp, (x0, y0, x1, y1) in zip(prps, bboxes)]
+    if show:
+        display()
 
     if len(measurements) < nlab:
         print("Skipped {} props with area < 10000".format(nlab - len(measurements)))
@@ -87,14 +131,9 @@ def algo_area(dpic, lpic, nlab, centering=None, show=0):
 
 
 def run(algorithm, **kw):
-    import os
-    from csxdata import roots
-    nopics = len([fl for fl in os.listdir(roots["ntabpics"])])
-
     measured = []
     for i, (dpic, lpic, nlab) in enumerate(map(preprocess, pics), start=1):
-        pprint("Doing pic {0:>{w}}/{1} ".format(i, nopics, w=len(str(nopics))))
-        means = algorithm(dpic, lpic, nlab, kw.get("centering", np.mean), 1)
+        means = algorithm(dpic, lpic, nlab, **kw)
         print("\n".join(str(round(m, 3)) for m in means))
         measured.extend(means)
 
@@ -107,6 +146,5 @@ def run(algorithm, **kw):
     pprint("Total number of objects inspected: {}".format(len(measured)))
     return measured
 
-
 if __name__ == '__main__':
-    run(algo_fitpolynom)
+    run(algo_fitpolynom, show=True)
