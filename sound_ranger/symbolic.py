2a1c110190a04cc0c4185a47c81eff0be3c1c8be
Csxeba <csxeba@gmail.com>
2017-03-15 13:12:18 +0100

    Reimplemeted

--- sound_ranger/symbolic.py
+++ sound_ranger/symbolic.py
@@ -1,21 +1,60 @@
-from sympy import symbols, cos, simplify, pprint, acos, pi
+from sympy import symbols, cos, simplify, pprint, sqrt, acos, pi
 from sympy.solvers import solve
 
-R, B, A, b, a, beta = symbols(
-    ("R", "B", "A", "b", "a", "beta"))
 
-# R = (-A**2/2 + B**2/2 - B*a*cos(theta) + a**2/2)/(A - B + a*cos(theta))
-# R = B*(B**2 + b**2 - b*cos(beta - theta))/(B + b*cos(beta - theta))
+"""
+equation_1: R**2 = (R+B)**2 + b**2 + 2*b*(R+B)*cos(theta-beta)
+solved for R: R = sqrt((R+B)**2 + b**2 + 2*b*(R+B)*cos(theta-beta))
 
-theta = -acos((-A**2 - 2*A*R + B**2 + 2*B*R + a**2)/(2*a*(B + R))) + 2*pi
+equation_2: (R+A)**2 = (R+B)**2 + a**2 + 2*a*(R+B)*cos(theta)
+solved for R: R = sqrt((R+B)**2 + a**2 + 2*a*(R+B)*cos(theta) - 2*R*A - A**2)
+"""
 
-# R1 = ((2*R*B + B**2 + a**2 - 2*a*cos(theta)*(R+B) - A**2) / (2*A)) - R
-R2 = (B**2 + b**2 - 2*b*cos(beta - theta)*(R+B) / (2*B)) - R
+# These are the constants
+B, A, b, a, beta = symbols(("B", "A", "b", "a", "beta"))
+# These are the variables
+R, theta = symbols(("R", "theta"))
 
-pprint(simplify(R2))
-print(solve(R2, "theta"))
+R_equation_1 = sqrt((R+B)**2 + b**2 + 2*b*(R+B)*cos(theta-beta))
+R_equation_2 = sqrt((R+B)**2 + a**2 + 2*a*(R+B)*cos(theta) - 2*R*A - A**2)
 
-# theta = -acos((-A**2 - 2*A*R + B**2 + 2*B*R + a**2)/(2*a*(B + R)))
-#         + 2*pi, acos((-A**2 - 2*A*R + B**2 + 2*B*R + a**2)/(2*a*(B + R)))
-# theta = beta - acos(B*(B**2 - R + b**2)/(b*(B + R))), beta
-#         + acos(B*(B**2 - R + b**2)/(b*(B + R))) - 2*pi
+
+def solve_eq_for_theta(R_equation):
+    theta_1, theta_2 = solve(R_equation, "theta")
+    # produces:
+    # theta_1 = -acos((-A**2 + B**2 + a**2)/(2*B*a)) + 2*pi
+    # theta_2 = acos((-A**2 + B**2 + a**2)/(2*B*a))
+    return theta_1, theta_2
+
+
+def solve_the_other_equation(the_other_equation, old_theta, theta_expressed):
+    """
+    Does not work :( IDK how simpy operates,
+    so please use the concrete implementation!
+    """
+    substituted = simplify(the_other_equation.subs(old_theta, theta_expressed))
+    new_R_equation = simplify(solve(substituted, "R"))
+    return new_R_equation
+
+
+def concrete_implementation():
+    # th was expressed from R_equation_1
+    th = -acos((-A**2 + B**2 + a**2)/(2*B*a)) + 2*pi
+    # redefine R_equation_2 with th instead of theta
+    re_R2 = sqrt((R + B)**2 + a**2 + 2*a*(R + B)*cos(th) - 2*R*A - A**2)
+    print("Theta eliminated:", simplify(re_R2))
+    print("Solved for R:", simplify(solve(re_R2, "R")))
+
+
+if __name__ == '__main__':
+    # The commented-out section is using the abstract implementation
+    # --------------------------------------------------------------
+    # th1, th2 = solve_eq_for_theta(R_equation_1)
+    # print("theta_1 = ", th1)
+    # print("theta_2 = ", th2)
+    # new_R1 = solve_the_other_equation(R_equation_2, old_theta=theta, theta_expressed=th1)
+    # print("new_R1 = ", new_R1)
+    # new_R2 = solve_the_other_equation(R_equation_2, old_theta=theta, theta_expressed=th2)
+    # print("new_R2 = ", new_R2)
+
+    concrete_implementation()
