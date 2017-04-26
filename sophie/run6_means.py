04ee3a462aa024a2bdcf515a7107dceadeaf68cc
csa <csxeba@gmail.com>
2017-04-26 15:55:57 +0200

    SophieStat final plots

--- sophie/run6_means.py
+++ sophie/run6_means.py
@@ -21,28 +21,42 @@ def split_by_CCode(param):
     return new
 
 
-def plotem(byCCode, pnm):
+def correlate(pnm):
+    param = globals()[pnm]
+    X, Y = discard_NaN_rows(param, Y_C)
+    return stats.spearmanr(X, Y)
+
+
+def plotem(byCCode, pnm, absolute):
     annot = sorted([n for n in byCCode])
     X, std, Y = np.array([byCCode[n] for n in annot]).T
     ax = plt.gca()
     # ax.errorbar(Y, X, yerr=std*2, fmt="bX",
     #             ecolor="red", elinewidth=1,
     #             capsize=4)
-    ax.scatter(Y, X, color="blue", marker="X")
+    if absolute:
+        Y = np.abs(Y)
+    ax.scatter(Y, X, color="black", marker="o", s=7)
     fit = np.polyfit(Y, X, 1)
     lin = np.poly1d(fit)
     ax.plot(Y, lin(Y), "r-")
-    r, p = stats.pearsonr(X, lin(Y))
+    r, p = correlate(pnm)
     for label, y, x in zip(annot, Y, X):
         ax.annotate(label, xy=(y, x), xycoords="data",
                     horizontalalignment="right",
                     verticalalignment="top")
-    ax.set_xlabel("Y koordináta")
-    ax.set_ylabel(pnm)
-    plt.title("Országos {} átlagértékek\n$r^2 = {:.2f}$, $p = {:.2f}$, {}szignifikáns"
-              .format(pnm, r**2, p, "nem " if p > 0.05 else ""))
+    ax.set_xlabel("Egyenlítőtől való távolság")
+    ax.set_ylabel(axtitles[pnm])
+    ttl = "\n".join((
+        "Országos {} átlagértékek",
+        "Spearman rang-korreláció: $r = {:.2f}$, $p = {:.2f}$, {}szignifikáns"
+    ))
+    plt.title(ttl.format(axtitles[pnm], r**2, p, "nem " if p > 0.05 else ""))
+    plt.grid(True)
     plt.show()
 
+
+axtitles = {"DHI": "$(D/H)_I$ (ppm)", "D13C": "$\\delta^{13}C$ (‰)"}
 
 data, labels, header = parse_csv(projectroot + "01GEO.csv", indeps=2, headers=1,
                                  dehungarize=True, decimal=True, sep="\t")
@@ -53,5 +67,5 @@ CCode = labels[:, 0]
 
 dh1cc = split_by_CCode(DHI)
 d13cc = split_by_CCode(D13C)
-plotem(dh1cc, "$(D/H)_I$")
-plotem(d13cc, "r$\delta^{13}C$")
+plotem(dh1cc, "DHI", absolute=True)
+plotem(d13cc, "D13C", absolute=False)
