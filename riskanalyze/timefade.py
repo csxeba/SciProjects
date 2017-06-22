85009797ce48e36275dfb4edf2999fba2d8a4311
csa <csxeba@gmail.com>
2017-06-22 16:05:39 +0200

    Working on time-window display of points

--- riskanalyze/timefade.py
+++ riskanalyze/timefade.py
@@ -19,12 +19,12 @@ dmin, dmax = date.min(), date.max()
 tdelta = (date - date.min()).astype(int)
 alphas = np.ones_like(tdelta) * (tdelta / tdelta.max())
 red_rgba = np.zeros((alphas.size, 4))
-red_rgba[:, 2] = 1
+red_rgba[:, 0] = 1
 red_rgba[:, 3] = alphas
 
 xmin, ymin = xy.min(axis=0)
 xmax, ymax = xy.max(axis=0)
-plt.scatter(*xy.T, c=red_rgba)
+plt.scatter(*xy.T, c=red_rgba, s=3)
 # plt.scatter(*sxy["1"].T, color=date, s=3)
 # plt.scatter(*sxy["0"].T, color="red", s=3)
 plt.show()
