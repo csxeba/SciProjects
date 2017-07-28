5b926cab6fbef8e14476483febb7a5e4b8fc0672
csa <csxeba@gmail.com>
2017-07-28 11:04:20 +0200

    Fixed a bug

--- hippocrates/newXP/assemble_data.py
+++ hippocrates/newXP/assemble_data.py
@@ -5,15 +5,22 @@ from PIL import Image
 
 
 def pull_sequence_to_array(root):
-    ar = np.array([np.asarray(Image.open(root+"/"+fl)) for fl in os.listdir(root) if fl[-3:] == "bmp"])
+    arz = []
+    for fl in (flnm for flnm in os.listdir(root) if flnm[-3:] == "bmp"):
+        ar = np.asarray(Image.open(root+"/"+fl)).sum(axis=2, keepdims=True)
+        if ar.shape != [894, 661, 1]:
+            print("Invalid DIM:", ar.shape)
+            return None
+        arz.append(ar)
+    arz = np.stack(arz)
     switch = False
-    while len(ar) > 45:
-        ar = ar[:-1] if switch else ar[1:]
+    while len(arz) > 45:
+        arz = arz[:-1] if switch else arz[1:]
         switch = not switch
-    if len(ar) < 45:
-        print("Invalid number of frames!")
+    if len(arz) < 45:
+        print("Invalid ZDIM!")
         return None
-    return ar
+    return arz
 
 
 dataroot = os.path.expanduser("~/Prog/data/raw/mr/")
@@ -24,6 +31,6 @@ t2 = []
 for i, name in enumerate(sorted(names), start=1):
     print(f"{i}/{allnms} Reading {name}", end=" ")
     t2.append(np.array([pull_sequence_to_array(name+"/T2")]))
-print()
-t2 = np.array(t2)
+    print()
+t2 = np.array([tensor for tensor in t2 if tensor is not None])
 t2.dump("SM_T2pos.npa")
