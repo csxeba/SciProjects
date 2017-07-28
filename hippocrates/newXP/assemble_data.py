8b362fc15e99cc5ff90dde92f5dbb7d1ed94d3e8
csa <csxeba@gmail.com>
2017-07-28 13:15:18 +0200

    Preprocessor added

--- hippocrates/newXP/assemble_data.py
+++ hippocrates/newXP/assemble_data.py
@@ -1,36 +1,47 @@
 import os
+import pickle
 
 import numpy as np
 from PIL import Image
 
+dataroot = os.path.expanduser("~/Prog/data/raw/mr/")
 
-def pull_sequence_to_array(root):
+
+def pull_sequence_to_array(root, ds=1):
     arz = []
-    for fl in (flnm for flnm in os.listdir(root) if flnm[-3:] == "bmp"):
-        ar = np.asarray(Image.open(root+"/"+fl)).sum(axis=2, keepdims=True)
-        if ar.shape != [894, 661, 1]:
-            print("Invalid DIM:", ar.shape)
+    for fl in sorted(flnm for flnm in os.listdir(root) if flnm[-3:] == "bmp"):
+        ar = np.asarray(Image.open(root+"/"+fl))[..., 0][..., None].astype("uint8")
+        if ar.shape != (894, 661, 1):
+            print("Invalid DIM:", ar.shape, end="")
             return None
+        ar = ar[136:756, 72:572]
+        if ds > 1:
+            ar = ar[::ds, ::ds]
         arz.append(ar)
-    arz = np.stack(arz)
+
+    if not arz:
+        print("No pix!", end="")
+        return None
+    arz = np.stack(arz[:-4])
     switch = False
-    while len(arz) > 45:
+    while len(arz) > 40:
         arz = arz[:-1] if switch else arz[1:]
         switch = not switch
-    if len(arz) < 45:
-        print("Invalid ZDIM!")
+    if len(arz) < 40:
+        print("Invalid ZDIM!", len(arz), end="")
         return None
+    arz = arz.T
+    print("Final dim (x, y, z):", arz.shape, end=" ")
     return arz
 
 
-dataroot = os.path.expanduser("~/Prog/data/raw/mr/")
 os.chdir(dataroot)
 names = os.listdir(".")
 allnms = len(names)
 t2 = []
 for i, name in enumerate(sorted(names), start=1):
-    print(f"{i}/{allnms} Reading {name}", end=" ")
-    t2.append(np.array([pull_sequence_to_array(name+"/T2")]))
+    print(f"{i:>2}/{allnms} Reading {name}", end=" ")
+    t2.append(pull_sequence_to_array(name+"/T2", ds=1))
     print()
-t2 = np.array([tensor for tensor in t2 if tensor is not None])
+t2 = np.stack(t2)
 t2.dump("SM_T2pos.npa")
