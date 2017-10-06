ea19a6843a9f0ee5122734c0802577ba366e6bf3
csa <csxeba@gmail.com>
2017-10-06 07:31:15 +0200

    OMG I'm breaking everything!!!

--- rich/stockshu/ann.py
+++ rich/stockshu/ann.py
@@ -1,13 +1,7 @@
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import LSTM, Dense, Conv1D, MaxPool1D, Activation, BatchNormalization
-
-
-def pull_data(rescale=True):
-    from SciProjects.rich.stockshu.data_util import pull_data as _pd
-    stocks, header = _pd(rescale)
-    return stocks
+from keras.layers import LSTM, Dense, Conv1D, Activation, BatchNormalization, Flatten
 
 
 def data_stream(raw, timestep=7, batch_size=32):
@@ -29,10 +23,8 @@ def validation_data(raw, timestep=7, m=100):
 
 def build_LSTM(inputs, outputs):
     model = Sequential(layers=[
-        LSTM(120, activation="relu", input_shape=inputs),
-        BatchNormalization(),
-        Dense(30, activation="relu"),
-        BatchNormalization(),
+        LSTM(120, activation="relu", input_shape=inputs), BatchNormalization(),
+        Dense(30, activation="relu"), BatchNormalization(),
         Dense(outputs[0], activation="linear")
     ])
     model.compile(optimizer="adam", loss="mse")
@@ -41,16 +33,24 @@ def build_LSTM(inputs, outputs):
 
 def build_CNN(inputs, outputs):
     model = Sequential(layers=[
-        Conv1D(30, kernel_size=3), MaxPool1D(), Activation("relu"), BatchNormalization()
+        BatchNormalization(input_shape=inputs),
+        Conv1D(30, kernel_size=3), Activation("relu"), BatchNormalization(),
+        Conv1D(30, kernel_size=3), Activation("relu"), BatchNormalization(),
+        Conv1D(outputs[0], kernel_size=3),
+        Activation("linear"),
+        Flatten(),
     ])
+    model.compile(optimizer="adam", loss="mse")
+    return model
 
 
 def xperiment():
-    raw = pull_data()
-    N, D = raw.shape
-    Xstream = data_stream(raw, timestep=7, batch_size=32)
-    net = build_LSTM([7, D], [D])
-    net.fit_generator(Xstream, steps_per_epoch=(N*10)//32, epochs=10)
+    learnX, validX = pull_data()
+    N, D = learnX.shape
+    Xstream = data_stream(learnX, timestep=7, batch_size=32)
+    valid = validation_data(validX)
+    net = build_CNN([7, D], [D])
+    net.fit_generator(Xstream, steps_per_epoch=(N*100)//32, epochs=10., validation_data=valid)
 
 
 if __name__ == '__main__':
--- rich/stockshu/data_util.py
+++ rich/stockshu/data_util.py
@@ -1,13 +1,20 @@
+import numpy as np
 import pandas as pd
 
 from SciProjects.rich import projectroot
 
 
-def pull_data(rescale=True):
+def pull_data(rescale=True, crossval=0.1, shuffle=False):
     df = pd.read_excel(projectroot + "Filled.xlsx")
 
     stocks = df.as_matrix().astype(float)
     if rescale:
         stocks /= stocks.max(axis=0)
         stocks -= stocks.mean(axis=0, keepdims=True)
+
+        arg = np.arange(len(stocks))
+        if shuffle:
+            np.random.shuffle(arg)
+        return stocks[arg[crossval:]], stocks[arg[crossval:]]
+
     return stocks, df.columns
