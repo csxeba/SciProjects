db7a39c9a0c367993e505211f454657f3c5777dc
csa <csxeba@gmail.com>
2017-10-13 14:31:03 +0200

    Some debugging. Implementing MANOVA

--- rich/use_pdr/xperiment.py
+++ rich/use_pdr/xperiment.py
@@ -1,3 +1,19 @@
+import time
+import matplotlib.pyplot as plt
+
 import pandas_datareader as pdr
 
-print(pdr.get_data_yahoo("AAPL"))
+
+start = time.time()
+times = []
+try:
+    for i in range(10):
+        print(i+1)
+        pdr.get_data_yahoo("AAPL")
+        times.append(time.time() - start)
+except Exception as E:
+    print("Loop raised", str(E))
+finally:
+    print(f"TIMEREQ: {(time.time() - start)/len(times):.2f} s")
+    plt.plot(range(len(times)), times)
+    plt.show()
