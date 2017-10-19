3835c2663c872436761c20b423c68ef4f8681486
Csxeba <csxeba@gmail.com>
2017-10-19 05:56:20 +0200

    Added new datapoints

--- matt/analyse.py
+++ matt/analyse.py
@@ -1,14 +1,41 @@
 import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
-from csxdata import stats
+from csxdata.visual.scatter import Scatter2D
 
 from SciProjects.matt import projectroot
 
-df = pd.read_excel(projectroot + "Kozma.Izotóp.eredmény.zrg.xlsx")
 
-X = df.loc[:, "ETOH":"AMYL"]  # type: pd.DataFrame
-Y = df["GYUM"]  # type: pd.Series
+def plotdata(lX, Y, title):
+    plotter = Scatter2D(lX, Y, title=title, axlabels=["F01", "F02"])
+    plotter.split_scatter()
+    plt.legend()
+    plt.show()
 
-stats.correlation(X.dropna().as_matrix(), X.columns)
+
+df = pd.read_excel(projectroot + "adat.xlsx")
+
+X, Y = df.iloc[:, 1:].as_matrix(), df["GYUM"].as_matrix()
+
+pca = PCA(n_components=2)
+lda = LDA(n_components=2)
+
+pcaX = pca.fit_transform(X)
+ldaX = lda.fit_transform(X, Y)
+
+pca_title = f"PCA\nTotal explained variance: {pca.explained_variance_ratio_.sum():.2%}"
+lda_title = f"LDA\nTotal explained variance: {lda.explained_variance_ratio_.sum():.2%}"
+
+plotdata(pcaX, Y, pca_title)
+plotdata(ldaX, Y, lda_title)
+
+# x = X.dropna().as_matrix()
+#
+# full(x, names=X.columns)
+# for colname in X:
+#     fullplot(X[colname].dropna().as_matrix(), colname, histbins=7)
+#
+# stats.correlation(X.dropna().as_matrix(), X.columns)
 
