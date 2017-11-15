0bb88f6bf16ebde3ae3a1c47d3eb42c3260104f7
csa <csxeba@gmail.com>
2017-11-15 14:41:20 +0100

    Some experimentation

--- fruits/xperiment04/visualize_transforms.py
+++ fruits/xperiment04/visualize_transforms.py
@@ -1,24 +1,27 @@
-from sklearn.discriminant_analysis import (
-    LinearDiscriminantAnalysis as LDA
-)
-
 from csxdata.visual.scatter import Scatter3D
+
+from sklearn.decomposition import PCA, KernelPCA, FastICA, FactorAnalysis
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
 from SciProjects.fruits.fruitframe import FruitData
 
 
-def main():
+def transform(X, trname, y=None):
+    model = {"pca": PCA(whiten=True), "ica": FastICA(whiten=True),
+             "kpca": KernelPCA(kernel="rbf"), "lda": LDA(), "fa": FactorAnalysis()
+             }[trname.lower()]
+    return model.fit_transform(X, y)[:, :3]
+
+
+def plot_transform(trname, feature):
     df = FruitData(transform=True)
-    X, Y = df.volatile, df["FAMILIA"]
-    lda = LDA(n_components=3).fit(X, Y)
-    print("Expained variances:", lda.explained_variance_ratio_)
-    lX = lda.transform(X)
-    scat = Scatter3D(lX, Y.as_matrix(), axlabels=[
-        f"DF0{i} ({var:.2%})" for i, var in enumerate(lda.explained_variance_ratio_, start=1)
-    ])
+    X, Y = df.volatile, df[feature]
+    lX = transform(X, trname, Y)
+    scat = Scatter3D(lX, Y.as_matrix(), axlabels=[f"LatentFactor{i}" for i in range(1, 4)],
+                     title=f"Transformation: {feature.upper()}")
     # scat.scatter()
     scat.split_scatter()
 
 
 if __name__ == "__main__":
-    main()
+    plot_transform("fa", "FAMILIA")
